237c887dc7133fdea261d0ff0ed147e15216f1c1
siam114 <smsiamislam598@gmail.com>
2025-08-23 01:58:23 +0600

    design header part

--- src/components/CalculatorApp.jsx
+++ src/components/CalculatorApp.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 const CalculatorApp = () => {
   return (
     <React.Fragment>
       <section>
         <div className='bg-gray-900 w-96 h-screen mx-auto rounded-lg overflow-hidden mt-5'>
-            <div></div>
+            <div className='text-gray-200 w-full font-bold text-3xl h-16 flex items-center space-x-2 pl-2'>
+                <GiHamburgerMenu/>
+                <p>Calculator App</p>
+            </div>
         </div>
       </section>
     </React.Fragment>
