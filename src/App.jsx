c44740f7c6dd4ceda5ef2cf3c76890dcfbd0828f
siam114 <smsiamislam598@gmail.com>
2025-08-25 01:34:50 +0600

    create provider and export

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css'
 import CalculatorApp from './components/CalculatorApp'
+import {store} from './redux/store'
+import {Provider} from 'react-redux'
 
 function App() {
 
   return (
-    <React.Fragment>
-      <CalculatorApp/>
+      <React.Fragment>  
+      <Provider store={store}>
+      <CalculatorApp/>  
+      </Provider>
     </React.Fragment>
   )
 }
