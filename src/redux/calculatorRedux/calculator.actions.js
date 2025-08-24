06afbefa7e234abd244711612080e10adc3a5a13
siam114 <smsiamislam598@gmail.com>
2025-08-25 00:47:48 +0600

    action create and export

---
+++ src/redux/calculatorRedux/calculator.actions.js
@@ -0,0 +1,29 @@
+import { LOAD_ANS, LOAD_BACKSPACE, LOAD_BUTTONS, LOAD_CLEAR } from "./calculator.actionTypes"
+
+export const loadButtons = (number) => {
+    return {
+        type: LOAD_BUTTONS,
+        payload: number
+    }
+}
+
+export const loadAns = (number) => {
+    return {
+        type: LOAD_ANS,
+        payload: number
+    }
+}
+
+export const loadClear = (number) => {
+    return {
+        type: LOAD_CLEAR,
+        payload: number
+    }
+}
+
+export const loadBackspace = (number) => {
+    return {
+        type: LOAD_BACKSPACE,
+        payload: number
+    }
+}
