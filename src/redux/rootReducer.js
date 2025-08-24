1fe6f6365cd8fd0a973854aa4a7f80d2656d1f36
siam114 <smsiamislam598@gmail.com>
2025-08-25 01:24:45 +0600

    create root reducer

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,8 @@
+import {CALCULATOR_KEY, calculatorReducer} from "./calculatorRedux/calculator.reducer";
+import { combineReducers } from "redux";
+
+let rootReducer = combineReducers({
+    [CALCULATOR_KEY]: calculatorReducer,
+})
+
+export { rootReducer }
